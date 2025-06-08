4da6d3ac4e939f5be363aaa51d4a4032a1dea41e
Stoyan Haydushki <haydushki@uni-sofia.bg>
2025-06-08 11:18:10 +0300

    Add basic web UI elements

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from .config import config_by_name
 
@@ -11,8 +11,8 @@ def create_app(config_name='default'):
     from . import db
     db.init_app(app)
 
-    @app.route('/')
-    def index():
+    @app.route('/database')
+    def database():
         conn = db.get_db()
         cur = conn.cursor()
         try:
@@ -23,5 +23,13 @@ def create_app(config_name='default'):
             return f"Error connecting to database: {e}"
         finally:
             cur.close()
+
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
+    @app.route('/about')
+    def about():
+        return render_template("about.html")
 
     return app
