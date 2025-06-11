89eb4a26077f7fa7c8bbdac9bd8d1dc7b04499fd
Stoyan Haydushki <haydushki@uni-sofia.bg>
2025-06-11 18:28:25 +0300

    Add sitemap for debugging purposes

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 from .config import config_by_name
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 
 
 def create_app(config_name='default'):
@@ -34,4 +40,19 @@ def create_app(config_name='default'):
 
     from api.land_properties import land_properties
     app.register_blueprint(land_properties)
+
+    # Just for debug purposes
+    @app.route("/site-map")
+    def site_map():
+        links = []
+        for rule in app.url_map.iter_rules():
+            # Filter out rules we can't navigate to in a browser
+            # and rules that require parameters
+            if "GET" in rule.methods and has_no_empty_params(rule):
+                url = url_for(rule.endpoint, **(rule.defaults or {}))
+                links.append((url, rule.endpoint))
+        # links is now a list of url, endpoint tuples
+
+        return links
+
     return app
